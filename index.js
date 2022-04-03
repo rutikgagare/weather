42055abe4bd840a0f691b9f4a73e28c7839176fa
Rutik Gagare <rutikgagare091971@gmail.com>
2022-04-03 19:42:48 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const requests = require('requests');
+
+const homeFile = fs.readFileSync("home.html",'utf-8');
+
+const replaceVal = (tempVal, orgVal) =>{
+    let temperature = tempVal.replace("{%tempval%}", (orgVal.main.temp-273.15).toFixed(2));
+    temperature = temperature.replace("{%tempmin%}", (orgVal.main.temp_min-273.15).toFixed(2));
+    temperature = temperature.replace("{%tempmax%}", (orgVal.main.temp_max-273.15).toFixed(2));
+    temperature = temperature.replace("{%loaction%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+    return temperature;
+}
+
+const server = http.createServer((req,res) =>{
+    if(req.url == "/"){
+        requests("https://api.openweathermap.org/data/2.5/weather?q=pune&appid=6a1723e4c1b1f6c3d2cce9b2daf2b0f6")
+
+        .on('data', (chunk) => {
+            const objdata = JSON.parse(chunk);
+            const arrData = [objdata];
+            // console.log(arrData[0].main.temp);
+            const realTimeData = arrData.map((val) =>replaceVal(homeFile, val)).join("");
+            res.write(realTimeData);
+        })
+        .on('end', (err) => {
+            if (err) return console.log('connection closed due to errors', err);
+            // console.log('end');
+            res.end();
+        });
+    }
+});
+
+server.listen(8000, '127.0.0.1');
