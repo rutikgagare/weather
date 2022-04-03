8325907b4393c88951e8f064e337461f0ff9deae
Rutik Gagare <rutikgagare091971@gmail.com>
2022-04-03 22:48:44 +0530

    added scripts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const requests = require('requests');
+const port=process.env.PORT || 8000;
 
 const homeFile = fs.readFileSync("home.html",'utf-8');
 
@@ -33,4 +34,6 @@ const server = http.createServer((req,res) =>{
     }
 });
 
-server.listen(8000, '127.0.0.1');
+server.listen(port,()=>{
+    console.log("server created successfully!");
+});
